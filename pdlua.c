11ead18e3affc779fa6b59916f374b1e3b657df5
Seb Shader <sebfumaster@aol.com>
2023-01-11 18:35:19 -0800

    enable relative path loading

--- pdlua.c
+++ pdlua.c
@@ -464,9 +464,7 @@ static t_pdlua *pdlua_new
     PDLUA_DEBUG("pdlua_new: start with stack top %d", lua_gettop(__L));
     lua_getglobal(__L, "pd");
     lua_getfield(__L, -1, "_constructor");
-    /* ag: We need to use the basename of the object name here, in case a path
-       is specified (cf. pdlua_loader_pathwise below). */
-    lua_pushstring(__L, basename(s->s_name));
+    lua_pushstring(__L, s->s_name);
     pdlua_pushatomtable(argc, argv);
     PDLUA_DEBUG("pdlua_new: before lua_pcall(L, 2, 1, 0) stack top %d", lua_gettop(__L));
     if (lua_pcall(__L, 2, 1, 0))
@@ -1578,27 +1576,6 @@ static int pdlua_loader_fromfd
     t_pdlua_readerdata  reader;
 
     PDLUA_DEBUG("pdlua_loader: stack top %d", lua_gettop(__L));
-    // First check whether the class has already been created under a
-    // different path.
-    lua_getglobal(__L, "pd");
-    lua_getfield(__L, -1, "_check");
-    lua_pushstring(__L, name);
-    // We expect false here, indicating that the class hasn't been registered
-    // yet and thus we can safely go ahead and load the file. Everything else
-    // indicates an error. To be on the safe side, we also assume this if the
-    // call fails or returns anything else but a boolean value.
-    int res = lua_pcall(__L, 1, 1, 0) ||
-      !lua_isboolean(__L, -1) || lua_toboolean(__L, -1);
-    lua_pop(__L, 2);/* pop the result and the global "pd" */
-    // If res indicates an error, there's really nothing that we can do except
-    // bail out with failure. Everything else will leave the loader in an
-    // unstable state and wreak havoc further down the line. Thus we can't
-    // create the object even if the basename class was registered earlier.
-    if (res) {
-      pd_error(NULL, "lua: error loading `%s': class already exists", name);
-      return 0;
-    }
-    // If we come here we're good; go ahead and load the Lua source.
     class_set_extern_dir(gensym(dirbuf));
     pdlua_setrequirepath(__L, dirbuf);
     reader.fd = fd;
@@ -1635,7 +1612,20 @@ static int pdlua_loader_legacy
     fd = canvas_open(canvas, name, ".pd_lua", dirbuf, &ptr, MAXPDSTRING, 1);
     if (fd>=0)
     {
-      result=pdlua_loader_fromfd(fd, basename(name), dirbuf);
+      const char * basenamep = basename(name);
+      const int is_loadname = basenamep > name;
+      if (is_loadname)
+      {
+        lua_getglobal(__L, "pd");
+        lua_pushstring(__L, name);
+        lua_setfield(__L, -2, "_loadname");
+      }
+      result=pdlua_loader_fromfd(fd, basenamep, dirbuf);
+      if (is_loadname) {
+        lua_pushnil(__L);
+        lua_setfield(__L, -2, "_loadname");
+        lua_pop(__L, 1);
+      }
       sys_close(fd);
     }
     return result;
@@ -1660,14 +1650,25 @@ static int pdlua_loader_pathwise
     }
     /* ag: Try loading <path>/<classname>.pd_lua (experimental).
        sys_trytoopenone will correctly find the file in a subdirectory if a
-       path is given, and it will then return that subdir in dirbuf. Thus we
-       need to determine the basename of the script file so that it gets
-       loaded correctly. */
+       path is given, and it will then return that subdir in dirbuf. */
     fd = sys_trytoopenone(path, objectname, ".pd_lua",
       dirbuf, &ptr, MAXPDSTRING, 1);
     if (fd>=0)
     {
-      result=pdlua_loader_fromfd(fd, basename(objectname), dirbuf);
+      const char* basenamep = basename(objectname);
+      const int is_loadname = basenamep > objectname;
+      if (is_loadname)
+      {
+        lua_getglobal(__L, "pd");
+        lua_pushstring(__L, objectname);
+        lua_setfield(__L, -2, "_loadname");
+      }
+      result=pdlua_loader_fromfd(fd, basenamep, dirbuf);
+      if (is_loadname) {
+        lua_pushnil(__L);
+        lua_setfield(__L, -2, "_loadname");
+        lua_pop(__L, 1);
+      }
       sys_close(fd);
     }
     return result;
