920473b00f8e9f4dcc02879c16696c1dbfbc99e6
Albert Gräf <aggraef@gmail.com>
2023-01-14 07:00:24 +0100

    Revert "Merge dofile and doclassfile back into one."
    We really need to keep these separate since the externdir of a Lua class
    isn't necessarily the same as the canvas dir containing the object any
    more. The former would be used, e.g., in cases where you want to reload
    a class file for live coding purposes, whereas the latter would be used
    to load Lua code relative to the object's canvas dir (in particular,
    that would be the case for anonymous classes created using pdluax).
    
    This reverts commit f2a889c6ab822f9049ca5bba964c675a5f7e127e.

--- pd.lua
+++ pd.lua
@@ -333,6 +333,20 @@ function pd.Class:postinitialize() end
 
 function pd.Class:finalize() end
 
+function pd.Class:doclassfile(file)
+  -- in case of register being called, make sure
+  -- classes in other paths aren't getting affected
+  -- save old loadname in case of weird nesting loading
+  local namesave = pd._loadname
+  local pathsave = pd._loadpath
+  pd._loadname = nil
+  pd._loadpath = self._loadpath
+  local f, path = pd._doclassfile(self._class, file)
+  pd._loadname = namesave
+  pd._loadpath = pathsave
+  return f, path
+end
+
 function pd.Class:dofile(file)
   -- in case of register being called, make sure
   -- classes in other paths aren't getting affected
@@ -341,12 +355,7 @@ function pd.Class:dofile(file)
   local pathsave = pd._loadpath
   pd._loadname = nil
   pd._loadpath = self._loadpath
-  local f, path
-  if self._name == "pdlua" or self._name == "pdluax" then
-     f, path = pd._dofile(self._object, file)
-  else
-     f, path = pd._doclassfile(self._class, file)
-  end
+  local f, path = pd._dofile(self._object, file)
   pd._loadname = namesave
   pd._loadpath = pathsave
   return f, path
