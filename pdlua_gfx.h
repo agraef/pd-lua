e72befbf41259b17bfc42c33bd9508d82f76363a
Timothy Schoen <timschoen123@gmail.com>
2023-12-22 20:10:19 +0100

    Added GPL license to new files

--- pdlua_gfx.h
+++ pdlua_gfx.h
@@ -1,3 +1,25 @@
+/** @file pdlua_gfx.h
+ *  @brief pdlua_gfx -- an extension to pdlua that allows GUI rendering and interaction in pure-data and plugdata
+ *  @author Timothy Schoen <timschoen123@gmail.com>
+ *  @date 2023
+ *
+ * Copyright (C) 2023 Timothy Schoen <timschoen123@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
 
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
 #define PDLUA_OBJECT_REGISTRTY_ID 512
